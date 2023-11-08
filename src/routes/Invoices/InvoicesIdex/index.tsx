378e0168f6e3b6b55c266ba009ad793a812c17fe
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2023-11-08 00:20:16 -0300

    Index routes

---
+++ src/routes/Invoices/InvoicesIdex/index.tsx
@@ -0,0 +1,7 @@
+export default function InvoicesIndex() {
+    return (
+        <main style={{ padding: "1rem" }}>
+            <p>Invoices</p>
+        </main>
+    );
+}
---
+++ src/routes/Welcome/index.tsx
@@ -0,0 +1,7 @@
+export default function Welcome() {
+    return (
+        <main style={{ padding: "1rem" }}>
+            <p>Bem vindos</p>
+        </main>
+    );
+}
