0b93035b4fc732347000375eb099e788538eb0a0
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2023-11-08 15:47:53 -0300

    Search params

--- src/routes/Welcome/index.tsx
+++ src/routes/Welcome/index.tsx
@@ -2,6 +2,7 @@ export default function Welcome() {
     return (
         <main style={{ padding: "1rem" }}>
             <p>Bem vindos</p>
+            Link: https://reactrouter.com/en/v6.3.0/getting-started/tutorial
         </main>
     );
 }
